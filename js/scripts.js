e1fceb04516f22f322635b25a63955c82e49b520
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-02 12:43:20 -0700

    BUGFIX: now accounts for 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,44 @@
 // Utility Logic
 
-let numbers = /[1 - 3]/;
+let numbers = ["1", "2", "3"];
 function isaMatch(index) {
-  return index.toString().match(numbers);
+  return numbers.includes(index);
 }
 
 let transformedNumbers = [];
 
-function transormNumber(digit, length) {
+function transormNumber(digit) {
   const digitString = digit.toString();
-  let returnArray = [];
-  if (isaMatch(digitString)) {
-    const numberArray = digitString.split("");
-    //console.log(numberArray);
-    for (let i = 0; i <= length; i++) {
 
+  const numberArray = digitString.split("");
+  for (let i = 0; i <= numberArray.length; i++) {
+    if (isaMatch(numberArray[i])) {
       if (numberArray[i] === "1") {
-        returnArray.push("Beep!");
+        transformedNumbers.push("Beep!");
+        //break;
       } else if (numberArray[i] === "2") {
-        returnArray.push("Boop!");
+        transformedNumbers.push("Boop!");
+        //break;
       } else if (numberArray[i] === "3") {
-        returnArray.push("Won't you be my neighbor?");
-      } else {
-        console.log(numberArray[i]);
+        transformedNumbers.push("Won't you be my neighbor?");
+        //break;
       }
+    } else {
+      transformedNumbers.push(digitString);
     }
-  } else {
-    //console.log(digitString);
-    returnArray.push(digitString);
+    return transformedNumbers.join(" ");
   }
-  console.log(returnArray);
-  return returnArray.join(" ");
-  //console.log(numberArray);
 }
 
 function evaluateInputValue(textInput) {
   const inputNumbers = parseInt(textInput);
   let returnString = "";
   for (let i = 0; i <= inputNumbers; i++) {
-
-    returnString = returnString + transormNumber(i, textInput.toString().length);
+    returnString = transormNumber(i);
   }
   return returnString;
 }
-let inputValue = evaluateInputValue(50);
-//console.log(inputValue);
+let inputValue = evaluateInputValue(32);
+console.log(inputValue);
 // UI Logic
 
