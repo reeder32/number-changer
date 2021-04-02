5d2f41e9d54f8b35c87df9e7fcf1b98dae40318b
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-02 14:22:51 -0700

    BUGFIX: will transform numbers past 30

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,48 @@
 // Utility Logic
 
 let numbers = ["1", "2", "3"];
-function isaMatch(index) {
-  return numbers.includes(index);
-}
 
-let transformedNumbers = [];
+function isaMatch(numberArray) {
+  isIncluded = numberArray.some(function (e) {
+    //console.log(numbers.includes(e.toString()));
+    return numbers.includes(e.toString());
+  });
+  return isIncluded;
+}
 
 function transormNumber(digit) {
   const digitString = digit.toString();
-
   const numberArray = digitString.split("");
+  //console.log(digitString, numberArray);
+  let str = ""
   for (let i = 0; i <= numberArray.length; i++) {
-    if (isaMatch(numberArray[i])) {
-      if (numberArray[i] === "1") {
-        transformedNumbers.push("Beep!");
-        //break;
-      } else if (numberArray[i] === "2") {
-        transformedNumbers.push("Boop!");
-        //break;
-      } else if (numberArray[i] === "3") {
-        transformedNumbers.push("Won't you be my neighbor?");
-        //break;
-      }
-    } else {
-      transformedNumbers.push(digitString);
+
+    if (numberArray[i] === "1") {
+      str = str + "Beep! ";
+    } else if (numberArray[i] === "2") {
+      str = str + "Boop! ";
+    } else if (numberArray[i] === "3") {
+      str = str + "Won't you be my neighbor? "
     }
-    return transformedNumbers.join(" ");
   }
+  return str;
 }
 
 function evaluateInputValue(textInput) {
   const inputNumbers = parseInt(textInput);
   let returnString = "";
   for (let i = 0; i <= inputNumbers; i++) {
-    returnString = transormNumber(i);
+    const str = transormNumber(i);
+    if (str) {
+      returnString = returnString + str;
+    } else {
+      returnString = returnString + i.toString() + " ";
+    }
   }
   return returnString;
 }
-let inputValue = evaluateInputValue(32);
+let inputValue = evaluateInputValue(56);
 console.log(inputValue);
+
 // UI Logic
 
